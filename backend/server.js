fd164977bd626a79945b22e7a489928ade3fe1aa
Rohit030919 <garjerohit127@gmail.com>
2026-03-31 14:30:19 +0530

    fix mongoose connect and QR URL

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 const User = require("./models/User");
 require("dotenv").config();
 
-mongoose.connect("process.env.MONGO_URI")
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
 
@@ -114,7 +114,7 @@ app.get("/generateQR/:id", async (req, res) => {
   try {
     const batchId = req.params.id;
 
-    const url = `http://localhost:3000/batch/${batchId}`;
+    const url = `https://foodtrace-omega.vercel.app/batch/${batchId}`;
 
     const qrImage = await QRCode.toDataURL(url);
 
@@ -198,6 +198,8 @@ app.post("/admin/addUser", async (req, res) => {
   }
 });
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
